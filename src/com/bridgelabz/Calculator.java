a7a1f563e46c224e4f3016f2ffa19860825461ae
prakash-zodge-bridgelabz <prakash.zodge.bridgelabz@gmail.com>
2023-12-04 15:44:45 +0530

    Added: Multiplication

--- src/com/bridgelabz/Calculator.java
+++ src/com/bridgelabz/Calculator.java
@@ -6,11 +6,15 @@ public class Calculator {
         Calculator c1 = new Calculator();
         c1.UC_1_Addition(2,3);
         c1.UC_2_Subtraction(5,2);
+        c1.UC_3_Multiplication(3,5);
     }
     public void UC_1_Addition(double a, double b){
         System.out.println("Addition : "+(a+b));
     }
     public void UC_2_Subtraction(double a, double b){
         System.out.println("Substraction : "+(a-b));
+    }
+    public void UC_3_Multiplication(double a, double b){
+        System.out.println("Multiplication : "+(a*b));
     }
 }
