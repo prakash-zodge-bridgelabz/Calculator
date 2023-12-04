5c9eaa772e1c822119a4cb87825b86087ab29533
prakash-zodge-bridgelabz <prakash.zodge.bridgelabz@gmail.com>
2023-12-04 15:40:32 +0530

    Added: Substraction

--- src/com/bridgelabz/Calculator.java
+++ src/com/bridgelabz/Calculator.java
@@ -5,8 +5,12 @@ public class Calculator {
         System.out.println("Welcome to my calculator program");
         Calculator c1 = new Calculator();
         c1.UC_1_Addition(2,3);
+        c1.UC_2_Subtraction(5,2);
     }
     public void UC_1_Addition(double a, double b){
         System.out.println("Addition : "+(a+b));
+    }
+    public void UC_2_Subtraction(double a, double b){
+        System.out.println("Substraction : "+(a-b));
     }
 }
