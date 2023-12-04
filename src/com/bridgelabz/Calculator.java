99e22a41b26ae6f7d07ee80d8a9cd333160cebe3
prakash-zodge-bridgelabz <prakash.zodge.bridgelabz@gmail.com>
2023-12-04 15:37:35 +0530

    Added: Addition method

--- src/com/bridgelabz/Calculator.java
+++ src/com/bridgelabz/Calculator.java
@@ -3,5 +3,10 @@ package com.bridgelabz;
 public class Calculator {
     public static void main(String[] args) {
         System.out.println("Welcome to my calculator program");
+        Calculator c1 = new Calculator();
+        c1.UC_1_Addition(2,3);
+    }
+    public void UC_1_Addition(double a, double b){
+        System.out.println("Addition : "+(a+b));
     }
 }
